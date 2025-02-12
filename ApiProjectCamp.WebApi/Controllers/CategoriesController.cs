34ff58762f9909607ec477a7c9c642844f238a49
beste <beste@SERCAN>
2025-02-12 23:08:06 +0300

    Kategori API CRUD işlemleri tamamlandı.

--- ApiProjectCamp.WebApi/Controllers/CategoriesController.cs
+++ ApiProjectCamp.WebApi/Controllers/CategoriesController.cs
@@ -16,12 +16,49 @@ namespace ApiProjectCamp.WebApi.Controllers
             _context = context;
         }
 
+        //Listeleme İşlemi
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
+
+
+        //Ekleme İşlemi
         [HttpPost]
         public IActionResult CreateCategory(Category category)
         {
             _context.Categories.Add(category);
             _context.SaveChanges();
             return Ok("Kategori ekleme işlemi başarılı");
+        }
+
+        //Silme İşlemi
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori silme işlemi başarılı");
+        }
+
+        //ID'ye Göre Getirme İşlemi
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+
+        //Güncelleme İşlemi
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori güncelleme işlemi başarılı");
         }
     }
 }
