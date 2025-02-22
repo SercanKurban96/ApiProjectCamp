b9cf69626e3020ae56c982b148b2e3f407176575
Sercan KURBAN <sercankurban1996@gmail.com>
2025-02-22 13:47:13 +0300

    AutoMapper ve DTO ile CRUD işlemleri tamamlandı.

---
+++ ApiProjectCamp.WebApi/Dtos/FeatureDtos/CreateFeatureDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjectCamp.WebApi.Dtos.FeatureDtos
+{
+    public class CreateFeatureDto
+    {
+        public string FeatureTitle { get; set; }
+        public string FeatureSubTitle { get; set; }
+        public string FeatureDescription { get; set; }
+        public string FeatureVideoUrl { get; set; }
+        public string FeatureImageUrl { get; set; }
+    }
+}
---
+++ ApiProjectCamp.WebApi/Dtos/FeatureDtos/UpdateFeatureDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiProjectCamp.WebApi.Dtos.FeatureDtos
+{
+    public class UpdateFeatureDto
+    {
+        public int FeatureID { get; set; }
+        public string FeatureTitle { get; set; }
+        public string FeatureSubTitle { get; set; }
+        public string FeatureDescription { get; set; }
+        public string FeatureVideoUrl { get; set; }
+        public string FeatureImageUrl { get; set; }
+    }
+}
